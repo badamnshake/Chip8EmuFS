8430ab585b43ad6f8f41b7d5fdb84d005f44d391
Kartavya Gohil <83202563+badamnshake@users.noreply.github.com>
2023-12-11 19:11:34 +1100

    start work on dispaly and other modules

--- display.h
+++ display.h
@@ -1,5 +1,20 @@
 #pragma once
+
+#include <vector>
+
+using namespace std;
+
 class display
 {
+public:
+	display();
+	~display();
+
+public: 
+	int width = 64;
+	int height = 32;
+
+	// vector for filling the pixel by yes or no
+	vector<vector<bool>> color_matrix;
 };
 
